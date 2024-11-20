3da75c4a2c8528194baf7effc759a26a3e936c4f
Matty Serwer <Matthew.Serwer@gmail.com>
2024-11-20 18:41:28 -0500

    Adds link navigation styles

--- components/Sidebar/Sidebar.tsx
+++ components/Sidebar/Sidebar.tsx
@@ -1,18 +1,35 @@
+"use client";
+
 import Link from "next/link";
 import { FilesIcon, StarIcon } from "lucide-react";
 import { Button } from "@/components/ui/button";
-
+import { usePathname } from "next/navigation";
+import clsx from "clsx";
 const Sidebar = () => {
+
+  const pathname = usePathname();
   return (
     <div id="sidebar" className="w-36 flex flex-col gap-2 ml-4">
       <Link href="/dashboard/files">
-        <Button variant="ghost" className="w-full flex gap-2 justify-start">
+        <Button
+          variant={"link"}
+          className={clsx("w-full flex gap-2 justify-start",
+            {
+              "text-cyan-700": pathname.includes("/dashboard/files")
+            })}
+        >
           <FilesIcon /> All Files
         </Button>
       </Link>
 
       <Link href="/dashboard/favorites">
-        <Button variant="ghost" className="w-full flex gap-2 justify-start">
+        <Button
+          variant={"link"}
+          className={clsx("w-full flex gap-2 justify-start",
+            {
+              "text-cyan-700": pathname.includes("/dashboard/favorites")
+            })}
+        >
           <StarIcon /> Favorites
         </Button>
       </Link>
